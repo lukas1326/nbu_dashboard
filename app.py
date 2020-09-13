375ef4d4df56576485a078fd79d3720f762be4d4
lukas1326 <aprilium@live.com>
2020-09-13 12:29:56 +0300

    sidebar banks group

--- app.py
+++ app.py
@@ -10,16 +10,32 @@ from banks import banks
 
 
 
-st.title("Тема проекта")
+st.title("Показники фінансової діяльності банків України")
 st.sidebar.title("Фильтр по показателям и банкам")
 st.markdown("Здесь что-то можно написать")
-st.sidebar.markdown("И здесь что-то можно написать")
+st.sidebar.markdown("щось написати")
 
 
 name_pair=banks
-name_list=[i[0] for i in name_pair]
-choice = st.sidebar.multiselect('Pick bank', (name_list), key=0)
-
+name_list_A=[bank[0] for bank in banks if bank[2]=='A']
+st.sidebar.subheader("Банки з державною часткою")
+if st.sidebar.checkbox("Всі банки з державною часткою", False, key='0'):
+    choice_A = name_list_A
+else:
+    choice_A = st.sidebar.multiselect('Вибрати банк чи де-кілька банків', (name_list_A), key=1)
+st.sidebar.subheader("Банки іноземних банківських груп")
+name_list_B=[bank[0] for bank in banks if bank[2]=='B']
+if st.sidebar.checkbox("Всі банки іноземних банківських груп", False, key='0'):
+    choice_B = name_list_B
+else:
+    choice_B = st.sidebar.multiselect('Вибрати банк чи де-кілька банків', (name_list_B), key=1)
+st.sidebar.subheader("Банки з приватним капіталом")
+name_list_E=[bank[0] for bank in banks if bank[2]=='E']
+if st.sidebar.checkbox("Всі банки з приватним капіталом", False, key='0'):
+    choice_E = name_list_E
+else:
+    choice_E = st.sidebar.multiselect('Вибрати банк чи де-кілька банків', (name_list_E), key=1)
+choice = choice_A+choice_B+choice_E
 mfos=[i[1] for i in name_pair if i[0] in choice]
 
 
